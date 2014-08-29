79a1476b3d22aceebcad4f707e4c4ea42afa1412
Browny Lin <browny@bonopoints.com>
2014-08-29 13:23:53 +0800

    Separate view and controller

---
+++ app/main/controller.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+
+from flask import request, jsonify, make_response
+from flask import current_app
+import requests, json, re
+
+
+def query_by_string(stock_name):
+    api_key = current_app.config['GOOGLE_API_KEY']
+    search_engine_id = current_app.config['GOOGLE_SEARCH_ENGINE_ID']
+
+    QUERY = 'https://www.googleapis.com/customsearch/v1?key=%s&cx=%s&q='
+    FORMAT_QUERY = QUERY % (api_key, search_engine_id)
+    OPTIONAL_PARAMS = '&sort=date'
+
+    query = FORMAT_QUERY + stock_name + OPTIONAL_PARAMS
+    r = requests.get(query)
+    response_dict = r.json()
+
+    item_list = response_dict.get('items')
+    print 'all response keys: {}'.format(response_dict.keys())
+    print 'all item keys: {}'.format(item_list[0].keys())
+
+    return response_dict
+
+
+def get_date_from_snippet(snippet):
+
+    cyear  = '年'.decode("utf8")
+    cdate  = '日'.decode("utf8")
+    cmonth = '月'.decode("utf8")
+    cdate  = '日'.decode("utf8")
+
+    date = re.search('\d+' + cyear + '\d+'+ cmonth +'\d+' + cdate, snippet, 0)
+    if date:
+        print date.group()
+        return date.group()
+    else:
+        print snippet
+        return snippet
+
--- app/main/views.py
+++ app/main/views.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 
-from flask import Blueprint, request, jsonify, make_response
+from flask import Blueprint, jsonify, make_response
 from flask import current_app
-import requests, json, re
+import controller
 from .. import db
 from . import main
 
@@ -10,7 +10,7 @@ from . import main
 @main.route('/<stock_name>', methods=['GET'])
 def parse(stock_name):
 
-    response_dict = query_by_string(stock_name)
+    response_dict = controller.query_by_string(stock_name)
     item_list = response_dict.get('items')
 
     # for item in item_list:
@@ -18,41 +18,4 @@ def parse(stock_name):
     #     get_date_from_snippet(snippet)
 
     return jsonify(items=item_list)
-
-
-def query_by_string(stock_name):
-    api_key = current_app.config['GOOGLE_API_KEY']
-    search_engine_id = current_app.config['GOOGLE_SEARCH_ENGINE_ID']
-    QUERY = 'https://www.googleapis.com/customsearch/v1?key=%s&cx=%s&q='
-    FORMAT_QUERY = QUERY % (api_key, search_engine_id)
-    print FORMAT_QUERY
-
-    OPTIONAL_PARAMS = '&sort=date'
-
-    query = FORMAT_QUERY + stock_name + OPTIONAL_PARAMS
-    r = requests.get(query)
-    response_dict = r.json()
-
-    print 'all response keys: {}'.format(response_dict.keys())
-    item_list = response_dict.get('items')
-    print 'all item keys: {}'.format(item_list[0].keys())
-
-    return response_dict
-
-
-def get_date_from_snippet(snippet):
-
-    cyear  = '年'.decode("utf8")
-    cdate  = '日'.decode("utf8")
-    cmonth = '月'.decode("utf8")
-    cdate  = '日'.decode("utf8")
-
-    date = re.search('\d+' + cyear + '\d+'+ cmonth +'\d+' + cdate, snippet, 0)
-    if date:
-        print date.group()
-        return date.group()
-    else:
-        print snippet
-        return snippet
-
 
